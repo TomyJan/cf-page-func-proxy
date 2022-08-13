522e92add0ab9e5594c3b9981df1e7b600fba4a8
TomyJan <TomyJan6@gmail.com>
2022-08-13 15:10:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="107.175.142.72.sslip.io:8443";
+        url.hostname="107.175.142.72.sslip.io:5243";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
