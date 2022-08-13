cc8abff1980547b32756794c67a967bd03cd15c1
TomyJan <TomyJan6@gmail.com>
2022-08-13 15:08:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="https://down-od.vov.moe:8443";
+        url.hostname="107.175.142.72.sslip.io:8443";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
