0294752d7b5a559a5a6cd222427babf63014b8c6
TomyJan <TomyJan6@gmail.com>
2022-08-13 15:04:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="down-od.vov.moe";
+        url.hostname="down-od.vov.moe:8443";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
